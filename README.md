172024e46b232f719981af0a5dd7341661b359f7
Volodymyr Doskochinskiy <doskovol1@gmail.com>
2022-01-13 17:14:34 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# gulp-avif-css
+
+> Generates additional expressions with `.webp` & `.avif` classes and appropriate extension
+
+## Usage
+
+Install `gulp-avif-css` as a development dependency:
+
+```shell
+npm install -D gulp-avif-css
+```
+
+Add it to your `gulpfile.js`
+
+```javascript
+const gulp = require("gulp")
+const avifcss = require("gulp-avif-css")
+
+gulp.src("./src/css/*.css")
+    .pipe(avifcss())
+    .pipe(gulp.dest("./dist"))
+```
+
+Include special plugin adds `.avif` or `.webp` class to body (if it supports) into your JavaScript file (provided in HTML file)
+
+```javascript
+import "gulp-avif-css/plugin"
+```
+
+## Examples
+
+#### Input:
+
+```css
+.box {
+    background-image: url("image.png");
+}
+```
+
+#### Output:
+
+```css
+.box {
+    background-image: url("image.png");
+}
+
+.webp .box {
+    background-image: url("image.webp");
+}
+
+.avif .box {
+    background-image: url("image.avif");
+}
+```
+
+## Parameters
+
+### `extensions`
+Type: `Array`\
+Default: `["png", "jpg", "jpeg", "JPG", "JPEG"]`
+
+Sets fallback extensions
---
+++ index.js
@@ -0,0 +1,115 @@
+const through = require('through2')
+const css = require('css')
+
+class AvifCSS {
+  constructor() {
+    this.avaibleExtensions = ['png', 'jpg', 'jpeg', 'JPG', 'JPEG']
+  }
+
+  init = (extensions) => {
+    return through.obj((buffer, _, callback) => {
+      if (buffer.isNull()) {
+        return callback(null, buffer)
+      } else if (buffer.isStream()) {
+        return callback(null, buffer)
+      }
+
+      if (extensions) {
+        this.avaibleExtensions = extensions
+      }
+
+      this.buffer = buffer
+      this.fileData = buffer.contents.toString()
+      this.transform()
+
+      callback(null, this.buffer)
+    })
+  }
+
+  transform(rules = this.parseCSS()) {
+    rules.forEach((expression) => {
+      if (expression.type === 'media') {
+        return this.transform(expression.rules)
+      } else if (expression.type === 'rule') {
+        expression.declarations
+        .forEach((rule) => {
+          if (!this.isBackground(rule)) return
+
+          const imageExtension = this.getExtension(rule)
+
+          if (!imageExtension) return
+
+          this.createExpressions({
+            rule,
+            selectors: expression.selectors,
+            position: expression.type === 'media'
+              ? rule.position
+              : expression.position,
+            imageExtension
+          })
+        })
+      }
+    })
+
+    this.buffer.contents = new Buffer.from(this.fileData)
+  }
+
+  parseCSS() {
+    return css.parse(this.fileData).stylesheet.rules
+  }
+
+  isBackground({ property, value }) {
+    if (
+      property === 'background' ||
+      property === 'background-image' &&
+      value.indexOf('.webp') < 0 &&
+      value.indexOf('.avif') < 0
+    ) {
+      return true
+    }
+  }
+
+  getExtension({ value }) {
+    return this.avaibleExtensions.find((extension) => {
+      if (value.indexOf(extension) > 0) {
+        return extension
+      }
+    })
+  }
+
+  createExpressions({ rule, selectors, position, imageExtension }) {
+    const splitedData = this.fileData.split(/\n/)
+
+    const rowIndex = position.end.line - 1
+    const columnIndex = position.end.column - 1
+
+    const getSelector = (modernExtension) => {
+      return selectors.includes('body')
+        ? `${selectors.join('')}.${modernExtension}`
+        : `.${modernExtension} ${selectors.join('')}`
+    }
+
+    ;['avif', 'webp'].forEach((modernExtension) => {
+      splitedData.splice(rowIndex, 1, this.changeString(
+        splitedData[rowIndex],
+        columnIndex,
+        `
+          ${getSelector(modernExtension)} {
+            ${rule.property}: ${rule.value.replace(imageExtension, modernExtension)}
+          }
+        `.replaceAll('\n', '')
+        )
+      )
+    })
+
+    this.fileData = splitedData.join('\n')
+  }
+
+  changeString(string, changeIndex, changeText) {
+    return string.substring(0, changeIndex) + changeText + string.substring(changeIndex)
+  }
+}
+
+const avifcss = new AvifCSS()
+
+module.exports = avifcss.init
